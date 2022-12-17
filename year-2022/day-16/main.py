e3c1895dee819fd73ab01f95e2ea55ac9c9bf31a
Jacob Scott <jxscott@amazon.com>
2022-12-16 16:53:37 -0800

    year 2022 day 16 save 2

--- year-2022/day-16/main.py
+++ year-2022/day-16/main.py
@@ -1,4 +1,7 @@
-#! /home/jscott/.pyenv/shims/python
+#! /Users/jxscott/.pyenv/shims/python
+
+
+######! /home/jscott/.pyenv/shims/python
 
 from sys import argv
 from typing import List
@@ -15,13 +18,11 @@ def parse_line(line: str, tunnels):
 
     (tunnel_name, flow_rate, *tunnel_network) = clean_line.split(",")
 
-    # print((tunnel_name, flow_rate, tunnel_network))
-
     tunnels[tunnel_name] = {
         "name": tunnel_name,
         "flow_rate": int(flow_rate),
         "children": list(map(lambda x: x.strip(), tunnel_network)),
-        "open": False
+        "open": False,
     }
 
 
@@ -29,55 +30,75 @@ def calculate_total_release(flow_rate, time):
     return flow_rate * time
 
 
+# traverse the tunnels, keep track of the highest amount of flow that can be
+# released
 def traverse_tunnels(tunnels, start_tunnel, moves=30):
-    remaining_moves = moves
     queue = list()
-    queue.append(start_tunnel)
-    total_release = 0
+    # (tunnel_name, parent, total_released, moves_remaining, path_so_far)
+    queue.append((start_tunnel, None, 0, moves, [start_tunnel]))
+    maximum_release = float("-inf")
 
-    while len(queue) > 0 and remaining_moves != 0:
-        curr_tunnel = tunnels[queue.pop(0)]
-        print(f'move {remaining_moves} valve {curr_tunnel["name"]}')
+    while len(queue) > 0:
+        (tunnel_name,
+         parent_name,
+         total_released,
+         moves_remaining,
+         path) = queue.pop(0)
 
-        # how much pressure to spend on move opening the valve
-        open_current_release = 0
-        if curr_tunnel["open"]:
-            open_current_release = 0
-        else:
-            open_current_release = calculate_total_release(
-                curr_tunnel["flow_rate"], remaining_moves - 1)
+        if moves_remaining == 0:
+            print(path)
+            if total_released > maximum_release:
+                maximum_release = total_released
+            continue
 
-        child_releases = []
-        for child in curr_tunnel["children"]:
-            child_tunnel = tunnels[child]
-            if child_tunnel["open"]:
-                child_releases.append((child, 0))
-                continue
-            child_releases.append((child, calculate_total_release(
-                tunnels[child]["flow_rate"],
-                remaining_moves - 2
-            )))
+        curr_tunnel = tunnels[tunnel_name]
 
-        max_child_release = max(child_releases, key=lambda x: x[1])
-
-        # print(max_child_release, open_current_release)
-
-        # if we get more value in a child valve then go to that one and
-        # open it skipping this one, this costs a move
-        if max_child_release[1] > open_current_release:
-            queue.append(max_child_release[0])
-            remaining_moves -= 1
-        else:
-            # else open the current valve
+        if not curr_tunnel["open"] and curr_tunnel["flow_rate"] != 0:
+            # if the current valve isn't open then open it and spend a move
             curr_tunnel["open"] = True
-            queue.append(curr_tunnel["name"])
-            total_release += open_current_release
-            remaining_moves -= 1
+            queue.append((
+                tunnel_name,
+                None,  # could be good to go to the parent now
+                total_released + calculate_total_release(
+                    curr_tunnel["flow_rate"], moves_remaining - 1),
+                moves_remaining - 1,
+                [*path, tunnel_name]))
 
-    if remaining_moves < 30:
-        print("did not use all 30 moves")
+        # get the moves to each child node and add them to the stack
+        for child in curr_tunnel["children"]:
+            # it is never more efficient to immediately go back to the parent
+            if parent_name and parent_name == child:
+                continue
+            queue.append((
+                child,
+                tunnel_name,
+                total_released,
+                moves_remaining - 1,
+                [*path, child]
+            ))
 
-    return total_release
+    return maximum_release
+
+
+def foobar(tunnels, start_tunnel, minutes=30):
+    queue = []
+    queue.append((start_tunnel, 0, minutes))
+
+    while len(queue) > 0:
+        (current_tunnel_name, path_flow, remaining_minutes) = queue.pop(0)
+        current_tunnel = tunnels[current_tunnel_name]
+
+        # should I take the time to open this valve?
+        if not current_tunnel["open"] and current_tunnel["flow_rate"] != 0:
+            # open the valve
+            current_tunnel["open"] = True
+            flow_released = calculate_total_release(
+                current_tunnel["flow_rate"], remaining_minutes - 1)
+            queue.insert(0, (current_tunnel_name, path_flow +
+                         flow_released, remaining_minutes - 1))
+
+        for child in current_tunnel_name["children"]:
+            queue.insert(0, (child, remaining_minutes - 1))
 
 
 def prompt_one(input_lines: List[str]):
@@ -85,15 +106,33 @@ def prompt_one(input_lines: List[str]):
     start_tunnel = "AA"
 
     # build tunnel system as graph
-    for line in input_lines:
-        parse_line(line, tunnels)
+    # for line in input_lines:
+    #     parse_line(line, tunnels)
+    tunnels["AA"] = {
+        "name": "AA",
+        "flow_rate": 0,
+        "children": ["BB"],
+        "open": False
+    }
+    tunnels["BB"] = {
+        "name": "BB",
+        "flow_rate": 10,
+        "children": ["AA", "CC"],
+        "open": False
+    }
+    tunnels["CC"] = {
+        "name": "CC",
+        "flow_rate": 100,
+        "children": ["BB"],
+        "open": False
+    }
 
-    # pp(tunnels)
+    pp(tunnels)
 
     # traverse graph to find the most efficient way to spend 30 moves
-    answer = traverse_tunnels(tunnels, start_tunnel)
+    # answer = traverse_tunnels(tunnels, start_tunnel, 4)
 
-    return answer
+    # return answer
 
 
 def prompt_two(input_lines: List[str]):
